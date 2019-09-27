8f3a923404cfe599bc7b631914d99fbfb683842b
jocelyn <jocelyn@amtsolutions.fr>
2019-09-27 10:35:08 +0200

    create module helper

---
+++ index.js
@@ -0,0 +1,5 @@
+const logger = require('./lib/logger');
+const { Command, Field } = require('./lib');
+
+module.exports = { logger, Command, Field };
+module.exports.create = (props) => new Command(props);
