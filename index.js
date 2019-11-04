ec4113927e320390cdeeb96913e2f76e36f5760e
Jocelyn <jocelyn.faihy@gmail.com>
2019-11-04 17:35:52 +0100

    change cli program

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-const { Command, Field, Program } = require('./lib');
+const program = require('caporal');
+const Program = require('./lib/models/program');
+const Command = require('./lib/models/program');
+const Field = require('./lib/models/program');
 
-module.exports = { Command, Field, Program };
+module.exports.Program = Program;
+module.exports.Command = Command;
+module.exports.Field = Field;
+
+module.exports.FLAGS = {
+    FLOAT: program.FLOAT,
+    INTEGER: program.INTEGER,
+    BOOLEAN: program.BOOLEAN,
+    ARRAY: program.ARRAY,
+    REPEATABLE: program.REPEATABLE,
+
+    BOOL: program.BOOL,
+    INT: program.INT,
+    LIST: program.LIST,
+};
+
 module.exports.create = (props) => {
     return new Program(props);
 };
-module.exports.configure = (props) => {
+module.exports.run = (props) => {
     const program = module.exports.create(props);
-    return program.configure();
+    return program.run();
 };
--- tests/echo.js
+++ tests/echo.js
@@ -1,29 +1,33 @@
 #!/usr/bin/env node
 'use strict';
 
-const Amiral = require('../index');
+const crieur = require('crieur');
+const Sergent = require('../index');
 
-Amiral.configure({
+Sergent.run({
     name: 'hello',
     description: 'Say hello',
     version: '0.0.0',
     fields: [
         { name: 'world', label: 'Say hello to world', type: Boolean, default: true },
-        { name: 'uppercase', label: 'Say hello with uppercase', shortcut: 'u', type: Boolean },
-        { name: 'lowercase', label: 'Say hello with lowercase', shortcut: 'l', type: Boolean },
+        { name: 'upper', label: 'Say hello with uppercase', shortcut: 'u', type: Boolean },
+        { name: 'lower', label: 'Say hello with lowercase', shortcut: 'l', type: Boolean },
     ],
     commands: [{
         name: 'to',
         description: 'Say hello to someone',
         fields: [
-            { name: 'name', label: 'Say hello to', argument: true },
+            { name: 'name', label: 'Say hello to', argument: true, required: true },
         ]
     }]
 }).then((command) => {
-    console.log(command.name);
-    command.prompt().then((props) => {
-        console.log(props);
-    });
+    const props = command.parse();
+    console.log(props);
+    let value = props.name || (props.world ? 'world' : 'you');
+    if (props.upper) value = value.toUpperCase();
+    if (props.lower) value = value.toLowerCase();
+
+    console.log(`Hello ${value}`);
 }).catch(error => {
-    throw error;
+    crieur.error(error);
 });
